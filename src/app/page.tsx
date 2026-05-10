03457109d840245c331e3a5259d591dd71ce7da5
AbdulShaikz <starabdul100@gmail.com>
2026-05-10 20:55:33 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return(
+    <main className="flex min-h-screen justify-center items-center">
+      <h1 className="md:text-5xl font-bold">TagFlow - Coming Soon.....</h1>
+    </main>
+  );
+}
