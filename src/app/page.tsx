e0958d53d72159781f0693bd615dcb57b928b12d
AbdulShaikz <starabdul100@gmail.com>
2026-05-13 20:45:44 +0530

    feat: implement NextAuth, GitHub OAuth, and session UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+  const {data: session, status} = useSession();
+
+  if(status=="loading") return <p className="flex min-h-screen justify-center items-center">Loading...</p>
+
+  if(!session){
+    return(
+      <main className="flex flex-col min-h-screen justify-center items-center gap-4">
+        <h1 className="md:text-5xl font-bold">TagFlow</h1>
+        <p className="text-gray-500">Your AI-powered bookmark manager</p>
+        <button
+          onClick={()=>signIn("github")}
+          className="rounded bg-blue-600 px-4 py-2 text-white cursor-pointer"
+        >
+          Sign in with GitHub
+        </button>
+      </main>
+    );
+  }
+
   return(
-    <main className="flex min-h-screen justify-center items-center">
-      <h1 className="md:text-5xl font-bold">TagFlow - Coming Soon.....</h1>
+    <main className="flex min-h-screen flex-col justify-center items-center gap-4">
+      <h1 className="text-4xl font-bold">TagFlow</h1>
+      <div className="flex items-center gap-4">
+        {session.user?.image && (
+          <img src={session.user.image} alt="avatar" className="w-10 h-10 rounded-full"/>
+        )}
+        <p>Welcome, {session.user?.name} ({session.user?.email})</p>
+      </div>
+      <button
+        onClick={()=>signOut()}
+        className="rounded bg-red-500 px-4 py-2 text-white"
+      >
+        Sign out
+      </button>
     </main>
   );
 }
